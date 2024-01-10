7a0fcf0d4752fb198730a03cc0e5481d52afd3b3
neelam <hjangra751@gmail.com>
2024-01-10 22:46:41 +0530

    digital-assess app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
+import  'bulma/css/bulma.css'
 import ProfileCard from "./ProfileCard";
+import AlexaImage from  "./img/alexa.png";
+import CortanaImage from "./img/cortana.png";
+import SiriImage from "./img/siri.png";
+
 function App(){
     return(
         <div>
-            <section class="hero is-link">
-              <div class="hero-body">
-                 <p class="title"> personal digital assistant</p>
-            </div>
-            </section>     
-        <div className ="container">
-            <section className ="section">
-            <div className = "column">
-                <div className = "column is -4">
-                <ProfileCard title="siri"
-                 handle = "siri45"></ProfileCard>
-                    </div>
-                    
-                      <ProfileCard  title="cortana" handle = "cortana99"></ProfileCard>
-                    <div className = "column is -4">
-                    </div> 
-                       <ProfileCard  title="alexa" handle = "alexa76"></ProfileCard>
-
-                    <div className = "column is -4">
+            <section className = "hero is-primary ">
+                <div className ="hero body">
+                    <p className ="title">
+                    <h1> Personal Digital Assistant</h1>
+                    </p>
                 </div>
-
+            </section>
+            <div className = "container">
+                <section className = "section">
+                    <div className = "column">
+                        <div className = "column is-3">
+                          <ProfileCard title = "Alexa"
+                           handle = "@alexa99"
+                           image={AlexaImage}
+                           description = "Alexa was created by amazon and it helps to buy things"
+                            />
+                           </div>
+                            <div className = "column is-3">
+                                 <ProfileCard title = "cortana" 
+                                 handle = "@cortana32" 
+                                 image = {CortanaImage}
+                                 description = "Cortana was created by microsoft .who know whats it works?"
+                                 />
+                                 </div>
+                                 <div className = "column is-3">
+                                    <ProfileCard title = "siri"
+                                     handle = "@siri01" 
+                                     image = {SiriImage}
+                                     description = "Siri was created by apple and being phased out"
+                                     />
+                                 </div>
+                             </div>
+                             </section>
+                        </div>
                     </div>
-          </section>
-          </div>
-        
-        </div>
   
       
     )
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,17 +1,26 @@
-function ProfileCard({title,handle,image}){
+function ProfileCard({title,handle,image,description}){
     return(
-        <div ClassName = "card">
-            <div ClassName = "card-image">
-            <figure class="image is-4by3">    
-             <img src= {image} alt="pda logo"/>
-              </figure>
-                </div>
-                <div className="card-content"></div>
-                <div class="media-content">
-          <p className="title is-4" > {title}  </p>
-           <p className="handle is -4">{handle}</p>
+        <div>
+             <div className = "card"> 
+          <div className ="card-image">
+          <figure className = "image is-1by1">
+          <img src={image} alt = "logo"/>
+           </figure>
         </div>
+      
+        <div className = "card-content">
+            <div className = "media-content">
+                <p className ="title is-4"> {title}  </p>
+                <p className ="subtitle is-6">{handle}</p>
+           </div>
+                  <div className = "content">{description}</div>
         </div>
+       </div>
+      
+        
+        
+       </div>
+        
     )
 }
 export default ProfileCard
